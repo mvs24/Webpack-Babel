8ccdd8d125396d2ba6aa2f222a0f8a2216859a1c
Marius <Marius.Vasili@fti.edu.al>
2019-10-15 14:56:23 +0200

    webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,38 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
   entry: "./src/index",
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
+    filename: "bundle.js",
+    publicPath: 'build/'
   },
   module: {
     rules: [
-    //   {
-    //     use: "babel-loader",
-    //     test: /\.js$/
-    //   }
-    {
-        use: ['style-loader', 'css-loader'],
+      //   {
+      //     use: "babel-loader",
+      //     test: /\.js$/
+      //   }
+      {
+        loader: ExtractTextPlugin.extract({
+          loader: "css-loader"
+        }),
         test: /\.css$/
-    }
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: { limit: 40000 } //40KB
+          },
+          "image-webpack-loader"
+        ]
+      } 
     ]
-  }
+  },
+  plugins: [new ExtractTextPlugin("style.css")]
 };
 
 module.exports = config;
