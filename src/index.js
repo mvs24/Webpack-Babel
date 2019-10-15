53fd09069ed308026a4f0ba24303be3c6fd9fdc2
Marius <Marius.Vasili@fti.edu.al>
2019-10-15 16:04:17 +0200

    code splitting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const button = document.createElement('button')
 button.innerText = 'Click me for CODE SPLITTING'
 button.onclick = () => {
     System.import('./imageViewer.js').then(importedModule => {
+        // code splitting
         importedModule.default();
     })
 }
